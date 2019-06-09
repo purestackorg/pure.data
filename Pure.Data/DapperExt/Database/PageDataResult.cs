ecca99796e3f4f2efcc125370988d8f4eca3eabd
benson <benson@HAIER-PC>
2019-06-09 18:42:22 +0800

    PageDataResult modify

--- Pure.Data/DapperExt/Database/PageDataResult.cs
+++ Pure.Data/DapperExt/Database/PageDataResult.cs
@@ -8,14 +8,17 @@ namespace Pure.Data
     /// 分页数据结果
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public class PageDataResult<T> {
+    public class PageDataResult<T>
+    {
 
-        public static PageDataResult<T> Empty(int pageIndex, int pageSize) {
+        public static PageDataResult<T> Empty(int pageIndex, int pageSize)
+        {
 
-            return new PageDataResult<T>(pageIndex, pageSize, 0 , default(T));
+            return new PageDataResult<T>(pageIndex, pageSize, 0, default(T));
         }
 
-        public PageDataResult(int pageIndex,  int pageSize, int total , T data ) {
+        public PageDataResult(int pageIndex, int pageSize, int total, T data)
+        {
             if (pageIndex <= 0)
             {
                 pageIndex = 1;
@@ -30,5 +33,30 @@ namespace Pure.Data
         public int PageSize { get; set; }
         public int Total { get; set; }
         public T Data { get; set; }
+        public int TotalPageCount
+        {
+            get
+            {
+                int totalPageCount = (int)Math.Ceiling((double)Total / (double)PageSize);
+                totalPageCount = totalPageCount < 0 ? 0 : totalPageCount;
+                return totalPageCount == 0 ? 1 : totalPageCount;
+
+            }
+        }
+        public bool HasPrevious
+        {
+            get
+            {
+                return PageIndex > 1;
+            }
+        }
+        public bool HasNext
+        {
+            get
+            {
+                return PageIndex < TotalPageCount;
+            }
+        }
+
     }
 }
